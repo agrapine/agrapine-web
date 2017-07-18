f257d3f81c1172a6bf3b77d33ff3fd7dbdccd0d1
Alexandru R. Agrapine <scors.agra@gmail.com>
2017-07-18 11:58:53 +0100

    so....

--- src/main.js
+++ src/main.js
@@ -1,10 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Counter from './Counter';
+import App from './App';
  
-document.addEventListener('DOMContentLoaded', function() {
-  ReactDOM.render(
-    React.createElement(Counter),
-    document.getElementById('mount')
-  );
-});
+ReactDOM.render(<App/>, document.getElementById('root'));
